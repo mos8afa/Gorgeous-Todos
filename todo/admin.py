e0a933a18bc431ace189ddd533a46651a8cbce8a
Mustafa <imos8afa@gmail.com>
2025-09-04 17:45:13 +0300

    Solve the summernote error

--- todo/admin.py
+++ todo/admin.py
@@ -2,9 +2,9 @@ from django.contrib import admin
 from .models import ToDo
 from django_summernote.admin import SummernoteModelAdmin
 
-class SomeModelAdmin(SummernoteModelAdmin): 
+class ToDoAdmin(SummernoteModelAdmin): 
     summernote_fields = '__all__'
-    list_display = ['name','price_per_day']
+    list_display = ['name','created_at','is_done']
 
-admin.site.register(ToDo)
+admin.site.register(ToDo,ToDoAdmin)
 
