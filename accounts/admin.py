dc59ee19c0c31b7ade646970192de80b93fccabb
Mustafa <imos8afa@gmail.com>
2025-09-04 17:38:26 +0300

    register summernote in admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
 from .models import Profile
 
-admin.register(Profile)
+admin.site.register(Profile)
--- todo/admin.py
+++ todo/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import ToDo
+from django_summernote.admin import SummernoteModelAdmin
 
-admin.register(ToDo)
+class SomeModelAdmin(SummernoteModelAdmin): 
+    summernote_fields = '__all__'
+    list_display = ['name','price_per_day']
+
+admin.site.register(ToDo)
 
