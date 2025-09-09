6c228b34d68997da66578cd82cb6704d14caa62a
Mustafa <imos8afa@gmail.com>
2025-09-09 16:57:11 +0300

    edit the profile update endpoint

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 
 
 class ProfileSreializer(serializers.ModelSerializer):
+    user = serializers.PrimaryKeyRelatedField(read_only=True) 
     class Meta:
         model = Profile
         fields = '__all__'
@@ -14,3 +15,19 @@ class UserSreialzier(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id','username','first_name','last_name','email','profile']
+    
+    def update(self, instance, validated_data):
+       
+        profile_data = validated_data.pop('profile', {})
+        instance.username = validated_data.get('username', instance.username)
+        instance.first_name = validated_data.get('first_name', instance.first_name)
+        instance.last_name = validated_data.get('last_name', instance.last_name)
+        instance.email = validated_data.get('email', instance.email)
+        instance.save()
+
+        profile = instance.profile
+        profile.image = profile_data.get('image', profile.image)
+        profile.phone = profile_data.get('phone', profile.phone)
+        profile.save()
+
+        return instance
